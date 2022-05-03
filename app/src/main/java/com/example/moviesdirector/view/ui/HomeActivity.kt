f4cdda8e67a60ba755633d9c167304de796718e7
Wesley Vila Nova <wesleyvilanovadelimatorres@gmail.com>
2022-05-03 08:53:19 -0300

    Create SplashScreen

---
+++ app/src/main/java/com/example/moviesdirector/view/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviesdirector.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesdirector.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/moviesdirector/view/ui/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.moviesdirector.view.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import com.example.moviesdirector.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initSplashScreen()
+    }
+
+    private fun initSplashScreen() {
+        Handler(Looper.getMainLooper()).postDelayed(5000,5000){
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
