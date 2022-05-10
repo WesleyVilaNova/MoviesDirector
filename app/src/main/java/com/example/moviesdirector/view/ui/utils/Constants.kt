98192cbf81d48f0f0588e65b3eb52f3a3fe16e01
Wesley Vila Nova <92765775+WesleyVilaNova@users.noreply.github.com>
2022-05-10 09:32:27 -0300

    Update Constants.kt

--- app/src/main/java/com/example/moviesdirector/view/ui/utils/Constants.kt
+++ app/src/main/java/com/example/moviesdirector/view/ui/utils/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
         internal const val TRENDING = "trending/"
         internal const val TYPE = "movie/"
         internal const val TEMP = "week?"
-        internal const val API_KEY = "319f821ad80072b8c1dd98a08e31346c"
+        internal const val API_KEY = ""
         internal const val PAGE = "1"
         internal const val LANGUAGE = "pt-BR"
         internal const val PARAMETER = "append_to_response=images"
@@ -18,10 +18,10 @@ class Constants {
 // trending/
 // movie/
 // week?
-// api_key=319f821ad80072b8c1dd98a08e31346c&
+// api_key=&
 // page=1&
 // language=pt-BR&
 // append_to_response=images
-// https://api.themoviedb.org/3/trending/movie/week?api_key=319f821ad80072b8c1dd98a08e31346c&page=1&language=pt-BR
-// https://api.themoviedb.org/3/movie/829557?api_key=319f821ad80072b8c1dd98a08e31346c&page=1&language=pt-BR
+// https://api.themoviedb.org/3/trending/movie/week?api_key=&page=1&language=pt-BR
+// https://api.themoviedb.org/3/movie/829557?api_key=&page=1&language=pt-BR
 }
