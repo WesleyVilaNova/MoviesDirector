2f17194c1ba4f1ecf6742f701d24224ea1e1aaec
Wesley Nova Lima <wnovalim@emeal.nttdata.com>
2022-05-16 16:21:48 -0300

    coroutines for suspended API call

--- app/src/main/java/com/example/moviesdirector/view/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/moviesdirector/view/ui/viewmodel/MainViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import com.example.moviesdirector.view.ui.models.ModelResultDetails
 import com.example.moviesdirector.view.ui.models.Result
 import com.example.moviesdirector.view.ui.repository.MainRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,20 +22,21 @@ class MainViewModel(private val repository: MainRepository) : ViewModel() {
     val errorMsg: LiveData<String> = _errorMsg
 
     fun getListMovies() {
-        val requestAPI = repository.getListMovies()
+        CoroutineScope(Dispatchers.IO).launch {
+            val requestAPI = repository.getListMovies()
+            requestAPI.enqueue(object : Callback<ModelResultDetails?> {
+                override fun onResponse(
+                    call: Call<ModelResultDetails?>,
+                    response: Response<ModelResultDetails?>
+                ) {
+                    val resultado = response.body()
+                    resultado.let { _listMovies.postValue(resultado?.results) }
+                }
 
-        requestAPI.enqueue(object : Callback<ModelResultDetails?> {
-            override fun onResponse(
-                call: Call<ModelResultDetails?>,
-                response: Response<ModelResultDetails?>
-            ) {
-                val resultado = response.body()
-                resultado.let { _listMovies.postValue(resultado?.results) }
-            }
-
-            override fun onFailure(call: Call<ModelResultDetails?>, t: Throwable) {
-                _errorMsg.postValue(t.message)
-            }
-        })
+                override fun onFailure(call: Call<ModelResultDetails?>, t: Throwable) {
+                    _errorMsg.postValue(t.message)
+                }
+            })
+        }
     }
 }
